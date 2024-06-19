2447f10bad43e435e29394efc7493dfe95db340a
梨央Rio <rio-kurniawan@sakura-schutzstaffel.com>
2024-06-19 20:20:55 +0700

    edit the work experience and about

--- components/nav/Heading.js
+++ components/nav/Heading.js
@@ -6,7 +6,7 @@ export const Heading = () => {
   return (
     <header className={styles.heading}>
       <MyLinks />
-      <OutlineButton onClick={() => window.open("/resume-new-fix-17-2.pdf")}>
+      <OutlineButton onClick={() => window.open("/ds.pdf")}>
         My resume
       </OutlineButton>
     </header>
